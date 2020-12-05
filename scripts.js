259bef3ddefc4e209901222ce89ab79e00ca9384
Paula Artusso <paulartusso@gmail.com>
2020-12-05 12:39:12 -0300

    completo

--- scripts.js
+++ scripts.js
@@ -1,10 +1,21 @@
+let errors = false;
 const validateEmpty = (input, errorSelector) =>{
     if (input.value == "" ){
         input.classList.add("show-error");
         const nameErrorMessage = document.getElementById(errorSelector);
         nameErrorMessage.classList.remove("hidden");
-    } else {
-        console.log(input.value)
+        errors = true;
+    }
+}
+
+const validateEmail = (errorSelector) =>{
+    const mail = document.getElementById("email-input");
+    const acceptedValues = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(mail.value);
+    if(!acceptedValues){
+        mail.classList.add("show-error");
+        const nameErrorMessage = document.getElementById(errorSelector);
+        nameErrorMessage.classList.remove("hidden");
+        errors = true;
     }
 }
 
@@ -25,15 +36,49 @@ const load = () =>{
     const phone = document.getElementById("phone-input");
     const phoneMessage =  document.getElementById("phone-error");
     phone.addEventListener("keyup", () => clearErrors(phone, phoneMessage));
+    const email = document.getElementById("email-input");
+    const emailMessage =  document.getElementById("email-error");
+    email.addEventListener("keyup", () => clearErrors(email, emailMessage));
 }
 
 const sendData = () =>{
+    errors = false;
     const name = document.getElementById("name-input");
     validateEmpty(name, "name-error");
     const surname = document.getElementById("surname-input");
     validateEmpty(surname, "surname-error");
+    const date = document.getElementById("date-input");
+    validateEmail("email-error");
     const phone = document.getElementById("phone-input");
     validateEmpty(phone, "phone-error");
-    const comments = document.getElementById("comments").value;
-    console.log(comments);
+    const comments = document.getElementById("comments");
+    if(!errors) {
+        const radios = document.getElementsByName("gender");
+        const select = document.getElementById("country");
+        let gender;
+        for (var i = 0, length = radios.length; i < length; i++) {
+            if (radios[i].checked) {
+                gender = radios[i].value;
+                break;
+            }
+        }
+
+        showSuccessMessage();
+        console.log(`
+            Nombre: ${name.value} ${surname.value},
+            Fecha de nacimiento: ${date.value},
+            Genero: ${gender},
+            Pais: ${select.value},
+            Telefono: ${phone.value},
+            Comentarios: ${comments.value}
+        `)
+    }
+}
+
+const showSuccessMessage = () =>{
+    let container = document.getElementById("success");
+    let message = document.createElement("p");
+    message.innerHTML ="Informacion enviada con exito";
+    container.appendChild(message);
+    container.classList.remove("hidden");
 }
